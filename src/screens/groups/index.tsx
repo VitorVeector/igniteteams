84611a9af1817f0009e22f38a1cf7675a89189ff
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-10-10 08:53:19 -0300

    Organizando diretórios

---
+++ src/screens/groups/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Groups() {
+  return (
+    <View>
+      <Text>Groups</Text>
+    </View>
+  );
+}
