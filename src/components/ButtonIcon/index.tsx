97210ee6d3269d529e3f5f9bc69d4c5edec600bd
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-10-18 08:50:00 -0300

    ButtonIcon Dinâmico

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,16 @@
+import {TouchableOpacityProps} from 'react-native'
+import {MaterialIcons} from '@expo/vector-icons'
+import * as S from './styles'
+
+type ButtonIconProps = TouchableOpacityProps & {
+    icon: keyof typeof MaterialIcons.glyphMap;
+    type?: S.ButtonIconTypeStyleProps;
+}
+
+export const ButtonIcon = ({icon, type='PRIMARY', ...rest}: ButtonIconProps) => {
+    return (
+        <S.Container>
+            <S.Icon type={type} name={icon}/>
+        </S.Container>
+    )
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,14 +1,19 @@
 import * as S from './styles'
 import {Header} from '@components/Header' 
 import {Highlight} from '@components/Highlight' 
+import {ButtonIcon} from '@components/ButtonIcon' 
+import { Input } from '@components/Input'
 
 export const Player = () => {
     return (
         <S.Container>
             <Header showBackButton/>
-
             <Highlight title='Nome da turma' subtitle='adicione a galera e separe os times'/>
-            
+
+            <S.Form>
+                <Input placeholder='Nome da pessoa' autoCorrect={false}/>
+                <ButtonIcon icon='add'/>
+            </S.Form>
         </S.Container>
     )
 }
